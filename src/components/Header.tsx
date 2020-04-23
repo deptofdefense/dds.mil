0aa24bfa455964421b5e142baf213eb82a369ca5
Jeff Hackshaw <jeffrey.hackshaw@friends.dds.mil>
2020-04-23 11:54:41 -0400

    update header styles and include logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,65 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+import { FaBars } from "react-icons/fa";
 import { HeaderNavLink } from "components";
 
 interface Props {
   siteTitle: string;
 }
 
-export const Header: React.FC<Props> = ({ siteTitle }) => (
-  <header className="usa-header usa-header--basic" role="banner">
-    <div className="usa-nav-container">
-      <div className="usa-navbar">
-        <div className="usa-logo" id="extended-logo">
-          <em className="usa-logo__text">
-            <Link to="/" aria-label="Home">
-              {siteTitle}
-            </Link>
-          </em>
+export const Header: React.FC<Props> = ({ siteTitle }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "logo-banner.png" }) {
+        childImageSharp {
+          fixed(width: 100) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <header className="usa-header usa-header--basic" role="banner">
+      <div className="usa-nav-container">
+        <div className="usa-navbar">
+          <div className="usa-logo" id="extended-logo">
+            <div className="usa-logo__text">
+              <Link to="/" aria-label="Home">
+                <div className="display-flex flex-row flex-align-center">
+                  <Img fixed={data.file.childImageSharp.fixed} />
+                  <div className="display-flex flex-column margin-left-1">
+                    <div className="text-bold text-ls-1">DEFENSE</div>
+                    <div>
+                      <span className="text-semibold">DIGITAL </span>
+                      <span className="text-light">SERVICE</span>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </div>
+          <button className="usa-menu-btn">
+            <FaBars />
+          </button>
         </div>
-        <button className="usa-menu-btn">Menu</button>
+        <nav aria-label="Primary navigation" className="usa-nav">
+          <button className="usa-nav__close">
+            <img src="/assets/img/close.svg" alt="close" />
+          </button>
+          <ul className="usa-nav__primary usa-accordion">
+            <HeaderNavLink to="/about">About</HeaderNavLink>
+
+            <HeaderNavLink to="/media">Media</HeaderNavLink>
+
+            <HeaderNavLink to="/careers">Careers</HeaderNavLink>
+
+            <HeaderNavLink to="/portfolio">Portfolio</HeaderNavLink>
+          </ul>
+        </nav>
       </div>
-      <nav aria-label="Primary navigation" className="usa-nav">
-        <button className="usa-nav__close">
-          <img src="/assets/img/close.svg" alt="close" />
-        </button>
-        <ul className="usa-nav__primary usa-accordion">
-          <HeaderNavLink to="/about">About</HeaderNavLink>
-
-          <HeaderNavLink to="/media">Media</HeaderNavLink>
-
-          <HeaderNavLink to="/careers">Careers</HeaderNavLink>
-
-          <HeaderNavLink to="/portfolio">Portfolio</HeaderNavLink>
-        </ul>
-      </nav>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
--- src/components/HeaderNavLink.tsx
+++ src/components/HeaderNavLink.tsx
@@ -7,7 +7,11 @@ interface Props {
 
 export const HeaderNavLink: React.FC<Props> = ({ children, to }) => (
   <li className="usa-nav__primary-item">
-    <Link to={to} className="usa-nav__link" activeClassName="usa-current">
+    <Link
+      to={to}
+      className="usa-nav__link desktop:font-sans-md"
+      activeClassName="usa-current"
+    >
       <span>{children}</span>
     </Link>
   </li>
