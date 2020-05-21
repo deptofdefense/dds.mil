a5fb040b9054c9fe19dbf9f28cf7c1006847b078
Jeff Hackshaw <jeffrey.hackshaw@friends.dds.mil>
2020-05-21 14:30:08 -0400

    add site dap and configure google analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,12 @@
 import "./src/styles/index.scss";
 import "uswds";
+
+const env = process.env.NODE_ENV;
+const ctx = process.env.CONTEXT || env;
+
+export const onRouteUpdate = ({ location }) => {
+  if (ctx !== "production" || !window.gas) {
+    return null;
+  }
+  window.gas("send", "pageview", location.pathname);
+};
