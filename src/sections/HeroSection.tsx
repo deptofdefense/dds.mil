bc5ea8cbccaa845374acba1c36a417e9d5899eaa
david saeger <david@dds.mil>
2021-01-23 12:06:19 -0500

    display none below 640px

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -37,19 +37,6 @@ export const HeroSection: React.FC<HeroSectionData> = ({
         }
       />
       <div className={clsx("hero-inner", { "hero-inner-expanded": cta })}>
-        {/* {title && (
-          <div className="hero-card">
-            <h1>{title}</h1>
-            {subtitle && <h2>{subtitle}</h2>}
-            {cta && (
-              <ConditionalLink to={ctaLink!}>
-                <button tabIndex={-1} className="hero-card-cta">
-                  {cta}
-                </button>
-              </ConditionalLink>
-            )}
-          </div>
-        )} */}
       <div className="video">
           <Video
             videoSrcURL="https://player.vimeo.com/video/503607244?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
